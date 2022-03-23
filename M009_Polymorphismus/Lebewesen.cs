9294a6270f178d2a33b025168622f51e755fc5e0
ppedvLukasK <lukask@ppedv.de>
2022-03-23 14:17:16 +0100

    M009

---
+++ M009_Polymorphismus/Lebewesen.cs
@@ -0,0 +1,57 @@
+﻿namespace M009_Polymorphismus;
+
+public abstract class Lebewesen //Abstrakte Klasse: Kann kein Objekt erstellen und dient als Struktur für Unterklassen
+{
+	public string Name { get; set; }
+
+	public Lebewesen(string name)
+	{
+		Name = name;
+	}
+
+	public abstract void WasBinIch(); //Abstrakte Methode ohne Body, muss überschrieben werden
+
+	public virtual void WasBinIchVirtual() //virtual wird auch weitergegeben
+	{
+		Console.WriteLine("Lebewesen wurde erzeugt");
+	}
+}
+
+public class Mensch : Lebewesen //Mensch ist Lebewesen
+{
+	//Name Feld übernommen
+
+	public int Alter { get; set; }
+
+	public Mensch(string name, int alter) : base(name)
+	{
+		Alter = alter;
+		WasBinIchVirtual();
+	}
+
+	public sealed override void WasBinIchVirtual()
+	{
+		Console.WriteLine("Mensch wurde erzeugt");
+	}
+
+	public override void WasBinIch()
+	{
+		Console.WriteLine("Mensch wurde erzeugt");
+	}
+}
+
+public class Kind : Mensch //sealed funktioniert auch bei Klassen
+{
+	public int Groesse { get; set; }
+
+	public Kind(string name, int alter, int groesse) : base(name, alter)
+	{
+		Groesse = groesse;
+		Console.WriteLine("Kind wurde erzeugt");
+	}
+
+	//public override void WasBinIchVirtual() //sealed: kann nicht weitervererbt werden
+	//{
+	//	Console.WriteLine("Kind wurde erzeugt");
+	//}
+}
---
+++ M009_Polymorphismus/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace M009_Polymorphismus;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Lebewesen lw = new Mensch("Max", 45);
+		if (lw.GetType() == typeof(Lebewesen)) //Laufzeittypvergleich
+		{
+			Console.WriteLine("lw ist Lebewesen");
+		}
+
+		if (lw.GetType() == typeof(Mensch)) //Laufzeittypvergleich
+		{
+			Console.WriteLine("lw ist Mensch");
+		}
+
+		if (lw is Lebewesen)
+		{
+			Console.WriteLine("lw ist Lebewesen");
+		} //Typvergleich nur kurz
+
+		if (lw is Mensch)
+		{
+			Console.WriteLine("lw ist Mensch");
+		}
+
+		Console.Write("1: ");
+		lw.WasBinIchVirtual(); //Objekt vom Typ Methode -> Methode von Mensch
+		Console.Write("2: ");
+		lw.WasBinIch(); //Variable vom Typ Lebewesen -> Methode von Lebewesen
+
+		Mensch m = (Mensch) lw;
+		Console.Write("3: ");
+		m.WasBinIchVirtual(); //Objekt vom Typ Methode -> Methode von Mensch
+		Console.Write("4: ");
+		m.WasBinIch(); //Variable vom Typ Mensch -> Methode vom Mensch
+
+		List<Lebewesen> lebewesens = new List<Lebewesen>(); //Liste von Lebewesen, kann alle möglichen Lebewesen (Mensch, Kind) halten
+		lebewesens.Add(m);
+		lebewesens.Add(new Kind("Max", 11, 120));
+
+		foreach (Lebewesen leb in lebewesens) //Liste iterieren und dem Typ entsprechende Methoden aufrufen
+		{
+			if (leb is Mensch)
+			{
+				Mensch mensch = (Mensch) leb;
+				//...
+			}
+
+			if (leb is Kind)
+			{
+				Kind kind = (Kind)leb;
+				//...
+			}
+		}
+	}
+}
